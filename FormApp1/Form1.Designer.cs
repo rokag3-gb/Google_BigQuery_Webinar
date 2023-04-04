ac6febb65947ca5d92d79e8608019b6e86e09590
rokag3@gmail.com <rokag3@gmail.com>
2023-04-04 16:23:05 +0900

    FormApp1 에서 BigQuery 데이터를 읽고 쓰는 부분 테스트 완료

--- FormApp1/Form1.Designer.cs
+++ FormApp1/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btn조회 = new Button();
+            btnInsert = new Button();
             SuspendLayout();
+            // 
+            // btn조회
+            // 
+            btn조회.Location = new Point(86, 72);
+            btn조회.Name = "btn조회";
+            btn조회.Size = new Size(265, 81);
+            btn조회.TabIndex = 0;
+            btn조회.Text = "Get BigQuery Data Select";
+            btn조회.UseVisualStyleBackColor = true;
+            btn조회.Click += btn조회_Click;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(86, 205);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(265, 81);
+            btnInsert.TabIndex = 1;
+            btnInsert.Text = "Insert into BigQuery";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnInsert);
+            Controls.Add(btn조회);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
@@ -43,5 +67,8 @@
         }
 
         #endregion
+
+        private Button btn조회;
+        private Button btnInsert;
     }
 }
--- FormApp1/Form1.cs
+++ FormApp1/Form1.cs
@@ -1,5 +1,8 @@
+using Google.Apis.Bigquery.v2.Data;
+using Google.Apis.Bigquery.v2;
 using Google.Cloud.BigQuery.V2;
-using System.Reflection;
+using System.Data.SqlTypes;
+using System.Diagnostics;
 
 namespace FormApp1
 {
@@ -10,6 +13,13 @@ namespace FormApp1
             InitializeComponent();
         }
 
+        string projectId = "cloudmate-sdteam";
+        string projectName = "SD Team";
+        string datasetId = "ds01";
+        string datasetName = "ds01";
+        string tableId = "tb1";
+        string tableName = "tb1";
+
         private void Form1_Load(object sender, EventArgs e)
         {
             //System.Reflection.Assembly[] asm = AppDomain.CurrentDomain.GetAssemblies();
@@ -17,29 +27,43 @@ namespace FormApp1
             //string[] files = File.ReadAllLines(path);
             //var f = Secret.files;
 
-            BigQueryTableInsertRows BQ = new BigQueryTableInsertRows();
-            BQ.TableInsertRows();
+            //BigQuery_CRUD Bq = new();
+            //Bq.TableInsertRows(projectId: projectId, datasetId: datasetId, tableId: tableId);
+
+            //Google.Apis.Services.BaseClientService.Initializer init = new();
+
+            //BigqueryService bqservice = new BigqueryService(
+            //bqservice.ini
         }
-    }
 
-    public class BigQueryTableInsertRows
-    {
-        public void TableInsertRows(
-            string projectId = "cloudmate-sdteam",
-            string datasetId = "ds01",
-            string tableId = "tb1")
+        private void btn¡∂»∏_Click(object sender, EventArgs e)
         {
-            var credentials = Secret.cred_Bq_SDTeam;
-            BigQueryClient client = BigQueryClient.Create(projectId, credentials);
+            string query = @"select
+                        col1
+                        , colString
+                        , DATETIME(CURRENT_TIMESTAMP(), 'Asia/Seoul') as datetime_KST
+                        , DATETIME(CURRENT_TIMESTAMP(), 'America/Los_Angeles') AS datetime_LA
+                        , generate_uuid() as UUID
+                        , 'asd' as VAL1
+                    from `ds01.tb1`;";
 
-            var result = client.ExecuteQuery("select "
-                + "DATETIME(CURRENT_TIMESTAMP(), 'Asia/Seoul') as datetime_KST"
-                + ", DATETIME(CURRENT_TIMESTAMP(), 'America/Los_Angeles') AS datetime_LA"
-                + ", generate_uuid() as UUID"
-                + ", 'asd' as VAL1;"
-                , parameters: null);
+            BigQueryClient client = BigQueryClient.Create(projectId, Secret.credential_SDTeam);
 
-            BigQueryInsertRow[] rows = new BigQueryInsertRow[] {
+            var result = client.ExecuteQuery(query, parameters: null);
+
+            client.Dispose();
+
+            foreach (var row in result)
+            {
+                Debug.WriteLine($"{row["col1"]}: {row["colString"]}, {row["datetime_KST"]}, {row["datetime_LA"]}, {row["UUID"]}, {row["VAL1"]} views");
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            BigQueryClient client = BigQueryClient.Create(projectId, Secret.credential_SDTeam);
+
+            BigQueryInsertRow[] insert_rows = new BigQueryInsertRow[] {
                 // The insert ID is optional, but can avoid duplicate data
                 // when retrying inserts.
                 new BigQueryInsertRow(insertId: "row1") {
@@ -52,9 +76,70 @@ namespace FormApp1
                 }
             };
 
-            client.InsertRows(datasetId, tableId, rows);
+            client.InsertRows(datasetId, tableId, insert_rows);
 
             client.Dispose();
+        }
+    }
+
+    public class BigQuery_CRUD
+    {
+        public void TableInsertRows(string projectId, string datasetId, string tableId)
+        {
+            BigQueryClient client = BigQueryClient.Create(projectId, Secret.credential_SDTeam);
+
+            BigQueryInsertRow[] insert_rows = new BigQueryInsertRow[] {
+                // The insert ID is optional, but can avoid duplicate data
+                // when retrying inserts.
+                new BigQueryInsertRow(insertId: "row1") {
+                    { "col1", 100 },
+                    { "colString", "WA" }
+                },
+                new BigQueryInsertRow(insertId: "row2") {
+                    { "col1", 101 },
+                    { "colString", "Colorado" }
+                }
+            };
+
+            client.InsertRows(datasetId, tableId, insert_rows);
+
+            client.Dispose();
+        }
+
+
+        public void ExecuteSQL(BigqueryService bqservice, String ProjectID)
+        {
+            string sSql = "SELECT r.Dealname, r.poolnumber, r.loanid FROM [MBS_Dataset.tblRemitData] R left join each [MBS_Dataset.tblOrigData] o on R.Dealname = o.Dealname and R.Poolnumber = o.Poolnumber and R.LoanID = o.LoanID Order by o.Dealname, o.poolnumber, o.loanid limit 100000";
+
+            QueryRequest _r = new QueryRequest();
+            _r.Query = sSql;
+            QueryResponse _qr = bqservice.Jobs.Query(_r, ProjectID).Execute();
+
+            string pageToken = null;
+
+            if (_qr.JobComplete == false)
+            {
+                //job not finished yet! expecting more data
+                while (true)
+                {
+                    var resultReq = bqservice.Jobs.GetQueryResults(_qr.JobReference.ProjectId, _qr.JobReference.JobId);
+                    resultReq.PageToken = pageToken;
+                    var result = resultReq.Execute();
+
+                    if (result.JobComplete == true)
+                    {
+                        //WriteRows(result.Rows, result.Schema.Fields);
+                        pageToken = result.PageToken;
+                        if (pageToken == null)
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                List<string> _fieldNames = _qr.Schema.Fields.ToList().Select(x => x.Name).ToList();
+                //WriteRows(_qr.Rows, _qr.Schema.Fields);
+            }
         }
     }
 }
