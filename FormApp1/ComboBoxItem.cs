dcba1b8e2fb2095de020bea68455a75b94054224
rokag3@gmail.com <rokag3@gmail.com>
2023-04-15 23:30:32 +0900

    Host 목록 담을 용도로 클래스 생성

---
+++ FormApp1/ComboBoxItem.cs
@@ -0,0 +1,19 @@
+﻿namespace FormApp1
+{
+    public class ComboBoxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public ComboBoxItem(string text, object value)
+        {
+            Text = text;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
